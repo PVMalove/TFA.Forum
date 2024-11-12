389657194d2390f23745b6913dc7d35a2e8334dd
Pavel Malove <107810341+PVMalove@users.noreply.github.com>
2024-11-12 20:29:57 +0300

    up add e2e test

---
+++ backend/tests/E2ETests/TFA.Forum.E2E/ForumApiApplicationFactory.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Testcontainers.PostgreSql;
+using TFA.Forum.API;
+using TFA.Forum.Persistence;
+
+
+namespace TFA.Forum.E2E;
+
+public class ForumApiApplicationFactory : WebApplicationFactory<Program>, IAsyncLifetime
+{
+    private readonly PostgreSqlContainer dbContainer = new PostgreSqlBuilder().Build();
+
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        var configuration = new ConfigurationBuilder()
+            .AddInMemoryCollection(new Dictionary<string, string>
+            {
+                ["ConnectionStrings:DbConnection"] = dbContainer.GetConnectionString(),
+            }!)
+            .Build();
+        builder.UseConfiguration(configuration);
+        base.ConfigureWebHost(builder);
+    }
+
+    public async Task InitializeAsync()
+    {
+        await dbContainer.StartAsync();
+        var forumDbContext = new ApplicationDbContext(new DbContextOptionsBuilder<ApplicationDbContext>()
+            .UseNpgsql(dbContainer.GetConnectionString()).Options);
+        await forumDbContext.Database.MigrateAsync();
+    }
+
+    public new async Task DisposeAsync() => await dbContainer.DisposeAsync();
+}
---
+++ backend/tests/E2ETests/TFA.Forum.E2E/ForumEndpointsShould.cs
@@ -0,0 +1,41 @@
+﻿using System.Net.Http.Json;
+using FluentAssertions;
+
+namespace TFA.Forum.E2E;
+
+public class ForumEndpointsShould : IClassFixture<ForumApiApplicationFactory>
+{
+    private readonly ForumApiApplicationFactory factory;
+
+    public ForumEndpointsShould(ForumApiApplicationFactory factory)
+    {
+        this.factory = factory;
+    }
+
+    [Fact]
+    public async Task CreateNewForum()
+    {
+        using var httpClient = factory.CreateClient();
+
+        using var getInitialForumsResponse = await httpClient.GetAsync("api/v1.0/Forum");
+        var initialForums = await getInitialForumsResponse.Content.ReadFromJsonAsync<Forum.Domain.Entities.Forum[]>();
+        initialForums
+            .Should().NotBeNull().And
+            .Subject.As<Forum.Domain.Entities.Forum[]>().Should().BeEmpty();
+
+        using var response = await httpClient.PostAsync("api/v1.0/Forum/create_forum",
+            JsonContent.Create(new { title = "New forum" }));
+        response.Invoking(r => r.EnsureSuccessStatusCode()).Should().NotThrow();
+
+        var forum = await response.Content.ReadFromJsonAsync<Forum.Domain.Entities.Forum>();
+        forum
+            .Should().NotBeNull().And
+            .Subject.As<Forum.Domain.Entities.Forum>().Title.Should().Be("New forum");
+        
+        using var getForumsResponse = await httpClient.GetAsync("api/v1.0/Forum");
+        var forums = await getForumsResponse.Content.ReadFromJsonAsync<Forum.Domain.Entities.Forum[]>();
+        forums
+            .Should().NotBeNull().And
+            .Subject.As<Forum.Domain.Entities.Forum[]>().Should().Contain(f => f.Title == "New forum");
+    }
+}
