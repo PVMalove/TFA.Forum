5e8ba0bff16c73e2ecfb0fca4508404cf5cc0dd2
Pavel Malove <107810341+PVMalove@users.noreply.github.com>
2024-12-07 22:09:00 +0300

    fix e2e tests

--- backend/tests/E2ETests/TFA.Forum.E2E/ForumEndpointsShould.cs
+++ backend/tests/E2ETests/TFA.Forum.E2E/ForumEndpointsShould.cs
@@ -1,6 +1,7 @@
 ﻿using System.Net.Http.Json;
 using System.Text.Json;
 using FluentAssertions;
+using TFA.Forum.Application.Authentication;
 using TFA.Forum.Domain.DTO.Forum;
 
 namespace TFA.Forum.E2E;
@@ -21,14 +22,41 @@ public class ForumEndpointsShould : IClassFixture<ForumApiApplicationFactory>
     {
         const string forumTitle = "DDAB3629-0BD9-4842-9C70-310A51694ACC";
 
+        await SignInAfterSignOn();
         await VerifyForumDoesNotExist(forumTitle);
         await CreateForum(forumTitle);
         await VerifyForumExists(forumTitle);
     }
 
+    private async Task SignInAfterSignOn()
+    {
+        using var signOnResponse = await httpClient.PostAsync(
+            "api/v1.0/Account/sign_on", JsonContent.Create(new { login = "Test", password = "qwerty" }));
+        signOnResponse.IsSuccessStatusCode.Should().BeTrue();
+
+        var jsonResponse = await signOnResponse.Content.ReadAsStringAsync();
+        var createdUser = JsonSerializer.Deserialize<ApiResponse<User>>(jsonResponse, jsonOptions);
+        createdUser.Should().NotBeNull();
+        createdUser!.Errors.Should().BeNull();
+        
+        using var signInResponse = await httpClient.PostAsync(
+            "api/v1.0/Account/sign_in", JsonContent.Create(new { login = "Test", password = "qwerty" }));
+        signInResponse.IsSuccessStatusCode.Should().BeTrue();
+        
+        jsonResponse = await signOnResponse.Content.ReadAsStringAsync();
+        var signedInUser = JsonSerializer.Deserialize<ApiResponse<User>>(jsonResponse, jsonOptions);
+        
+        signedInUser.Should().NotBeNull();
+        signedInUser!.Errors.Should().BeNull();
+
+        signedInUser.Result.Should()
+            .NotBeNull().And
+            .BeEquivalentTo(createdUser.Result);
+    }
+
     private async Task VerifyForumDoesNotExist(string forumTitle)
     {
-        var response = await httpClient.GetAsync("api/v1.0/Forum");
+        var response = await httpClient.GetAsync("api/v1.0/Forums");
         response.EnsureSuccessStatusCode();
 
         var jsonResponse = await response.Content.ReadAsStringAsync();
@@ -42,7 +70,7 @@ public class ForumEndpointsShould : IClassFixture<ForumApiApplicationFactory>
     private async Task CreateForum(string forumTitle)
     {
         var createForumContent = JsonContent.Create(new { Title = forumTitle });
-        var response = await httpClient.PostAsync("api/v1.0/Forum/create", createForumContent);
+        var response = await httpClient.PostAsync("api/v1.0/Forums", createForumContent);
         
         response.EnsureSuccessStatusCode();
 
@@ -57,11 +85,15 @@ public class ForumEndpointsShould : IClassFixture<ForumApiApplicationFactory>
 
     private async Task VerifyForumExists(string forumTitle)
     {
-        var response = await httpClient.GetAsync("api/v1.0/Forum");
+        var response = await httpClient.GetAsync("api/v1.0/Forums");
         response.EnsureSuccessStatusCode();
 
-        var forums = await response.Content.ReadFromJsonAsync<ForumCreateDto[]>();
-        forums.Should().NotBeNull()
+        var jsonResponse = await response.Content.ReadAsStringAsync();
+        var result = JsonSerializer.Deserialize<ApiResponse<ForumCreateDto[]>>(jsonResponse, jsonOptions);
+        
+        result.Should().NotBeNull();
+        result!.Errors.Should().BeNull();
+        result.Result.Should().NotBeNull()
             .And.Contain(f => f.Title!.Equals(forumTitle));
     }
 }
--- backend/tests/E2ETests/TFA.Forum.E2E/TopicEndpointsShould.cs
+++ backend/tests/E2ETests/TFA.Forum.E2E/TopicEndpointsShould.cs
@@ -2,20 +2,16 @@
 using System.Net.Http.Json;
 using System.Text.Json;
 using FluentAssertions;
+using TFA.Forum.Application.Authentication;
 using TFA.Forum.Domain.DTO.Forum;
 
 namespace TFA.Forum.E2E;
 
-public class TopicEndpointsShould : IClassFixture<ForumApiApplicationFactory>, IAsyncLifetime
+public class TopicEndpointsShould(ForumApiApplicationFactory factory)
+    : IClassFixture<ForumApiApplicationFactory>, IAsyncLifetime
 {
-    private readonly HttpClient _httpClient;
-    private readonly JsonSerializerOptions _jsonOptions;
-
-    public TopicEndpointsShould(ForumApiApplicationFactory factory)
-    {
-        _httpClient = factory.CreateClient();
-        _jsonOptions = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
-    }
+    private readonly HttpClient httpClient = factory.CreateClient();
+    private readonly JsonSerializerOptions jsonOptions = new() { PropertyNameCaseInsensitive = true };
 
     public Task InitializeAsync() => Task.CompletedTask;
 
@@ -27,27 +23,57 @@ public class TopicEndpointsShould : IClassFixture<ForumApiApplicationFactory>, I
         var forumTitle = Guid.NewGuid().ToString();
         var forumId = await CreateForumAsync(forumTitle);
 
-        var responseMessage = await _httpClient.PostAsync(
-            $"api/v1.0/Forum/{forumId}/topic",
+        var responseMessage = await httpClient.PostAsync(
+            $"api/v1.0/Forums/{forumId}/topics",
             JsonContent.Create(new { title = "Hello world", content = "Hello world" })
         );
 
-        responseMessage.StatusCode.Should().Be(HttpStatusCode.InternalServerError);
+        responseMessage.StatusCode.Should().Be(HttpStatusCode.Forbidden);
     }
 
+    private async Task SignInAfterSignOn()
+    {
+        using var signOnResponse = await httpClient.PostAsync(
+            "api/v1.0/Account/sign_on", JsonContent.Create(new { login = "Test", password = "qwerty" }));
+        signOnResponse.IsSuccessStatusCode.Should().BeTrue();
+
+        var jsonResponse = await signOnResponse.Content.ReadAsStringAsync();
+        var createdUser = JsonSerializer.Deserialize<ApiResponse<User>>(jsonResponse, jsonOptions);
+        createdUser.Should().NotBeNull();
+        createdUser!.Errors.Should().BeNull();
+        
+        using var signInResponse = await httpClient.PostAsync(
+            "api/v1.0/Account/sign_in", JsonContent.Create(new { login = "Test", password = "qwerty" }));
+        signInResponse.IsSuccessStatusCode.Should().BeTrue();
+        
+        jsonResponse = await signOnResponse.Content.ReadAsStringAsync();
+        var signedInUser = JsonSerializer.Deserialize<ApiResponse<User>>(jsonResponse, jsonOptions);
+        
+        signedInUser.Should().NotBeNull();
+        signedInUser!.Errors.Should().BeNull();
+
+        signedInUser.Result.Should()
+            .NotBeNull().And
+            .BeEquivalentTo(createdUser.Result);
+    }
+    
     private async Task<Guid> CreateForumAsync(string title)
     {
+        await SignInAfterSignOn();
         var createForumContent = JsonContent.Create(new { Title = title });
-        using var response = await _httpClient.PostAsync("api/v1.0/Forum/create", createForumContent);
+        using var response = await httpClient.PostAsync("api/v1.0/Forums", createForumContent);
 
         response.EnsureSuccessStatusCode();
 
         var jsonResponse = await response.Content.ReadAsStringAsync();
-        var createdForumResult = JsonSerializer.Deserialize<ApiResponse<ForumCreateDto>>(jsonResponse, _jsonOptions);
+        var createdForumResult = JsonSerializer.Deserialize<ApiResponse<ForumCreateDto>>(jsonResponse, jsonOptions);
 
         createdForumResult.Should().NotBeNull();
         createdForumResult!.Errors.Should().BeNull();
-
+        
+        using var signOnResponse = await httpClient.PostAsync("api/v1.0/Account/sign_out", null);
+        signOnResponse.IsSuccessStatusCode.Should().BeTrue();
+        
         return createdForumResult.Result!.Id;
     }
 }
